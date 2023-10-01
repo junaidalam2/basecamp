a889375cffa76585b37e0d160e34fc3933f2394a
junaidalam2 <junaid.alam2@gmail.com>
2023-10-01 07:11:12 -0700

    added devise

--- Gemfile
+++ Gemfile
@@ -68,5 +68,6 @@ group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem "capybara"
   gem "selenium-webdriver"
+  gem 'devise', '~> 4.9', '>= 4.9.2'
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_25_224006) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_01_124912) do
   create_table "comments", force: :cascade do |t|
     t.string "comment"
     t.datetime "created_at", null: false
@@ -56,6 +56,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_25_224006) do
     t.string "profile"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
