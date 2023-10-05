5be9b22734eb454a40fd11c38a7ce233a4504a2e
junaidalam2 <junaid.alam2@gmail.com>
2023-10-04 22:21:03 -0700

    added fields to sign up page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+        #devise_parameter_sanitizer.for(:sign_up).push(:first_name, :last_name,:title, :department, :profile)
+        #devise_parameter_sanitizer.for(:account_update).push(:name, :surname, :email, :avatar)
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name,:title, :department, :profile])
+    end
+
 end
