0a880b0d2fd8e4444d4a8862f6aa4b8bbdbb6e09
junaidalam2 <junaid.alam2@gmail.com>
2023-10-13 21:19:41 -0700

    nested form for issues/comments

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -3,5 +3,5 @@ class Issue < ApplicationRecord
     belongs_to :user
     has_many :comment, dependent: :destroy
     validates :project_id, :short_description, :detailed_description, :steps_to_reproduce, :priority, presence: true
-    accepts_nested_attributes_for :comment, reject_if: proc { |att| att['comment'].blank? } #, allow_destroy: true
+    accepts_nested_attributes_for :comment, reject_if: proc { |att| att['comment'].blank? }, allow_destroy: true
 end
