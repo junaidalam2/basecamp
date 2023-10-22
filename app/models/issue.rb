38c79d8606ca5d3cdf1cb1dc9b4358d8891c880e
junaidalam2 <junaid.alam2@gmail.com>
2023-10-21 21:43:15 -0700

    nested forms - revising for cocoon and simple form

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,7 +1,7 @@
 class Issue < ApplicationRecord
     belongs_to :project
     belongs_to :user
-    has_many :comment, dependent: :destroy
+    has_many :comment, dependent: :destroy, inverse_of: :issue
     validates :project_id, :short_description, :detailed_description, :steps_to_reproduce, :priority, presence: true
     accepts_nested_attributes_for :comment, reject_if: proc { |att| att['comment'].blank? }, allow_destroy: true
 end
