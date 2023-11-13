9046609fe4ac8a0c64f37d4dfc6439204e71737f
junaidalam2 <junaid.alam2@gmail.com>
2023-11-13 06:21:43 -0800

    bootstrapped user edit form and buttons on multiple pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,10 +14,12 @@ class UsersController < ApplicationController
   # GET /users/new
   def new
     @user = User.new
+    @submit_user_button_text = "Create user"
   end
 
   # GET /users/1/edit
   def edit
+    @submit_user_button_text = "Update user"
   end
 
   # POST /users or /users.json
